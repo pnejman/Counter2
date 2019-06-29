13fa9010ae92b45c09f03c2fa4f9fe0280563801
pnejman <pnejman@BYDDTPENEJMAN01>
2019-06-29 15:09:05 +0200

    Add project files.

---
+++ Counter2/ConsoleUI.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter2
+{
+    class ConsoleUI
+    {
+        enum inputType { numeric, words}
+        bool useNumbers;
+
+
+        private void ReadData(bool useNumbers)
+        {
+
+        }
+
+        private void ConsoleUIMain()
+        {
+            Console.Write("Select input type:\r\n" +
+                          "1. Numeric\r\n" +
+                          "2. Words\r\n" +
+                          ">");
+            ConsoleKeyInfo choice = Console.ReadKey();
+
+            switch (choice.KeyChar)
+            {
+                case '1':
+                    this.useNumbers = false;
+                    break;
+
+                case '2':
+                    this.useNumbers = true;
+                    break;
+
+                default:
+                    Console.WriteLine("\r\nError. There is no such option. Program will exit now.");
+                    Console.ReadKey(true);
+                    return;
+            }
+
+        }
+
+        public ConsoleUI()
+        {
+            ConsoleUIMain();
+        }
+    }
+}
